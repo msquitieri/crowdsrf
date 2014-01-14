20f2fb542d43158a101a54ed67280ec6e5c2535b
squit <michael.squitieri@gmail.com>
2014-01-14 17:01:27 +0100

    now adds information from Foursquare to the local locations database

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,16 @@
 class Location < ActiveRecord::Base
-	def addFoursquareLocation 
-		
+	has_many :photos
+	validates :lat, :lng, :foursquare_id, :name, presence: true
+	validates :foursquare_id, uniqueness: true
+
+	def self.addFoursquareLocation (foursquare_location)
+		location_params = {};
+
+		location_params[:foursquare_id] = foursquare_location.id
+		location_params[:lat] = foursquare_location.location.lat
+		location_params[:lng] = foursquare_location.location.lng
+		location_params[:name] = foursquare_location.name
+
+		return Location.create(location_params)
 	end
 end
