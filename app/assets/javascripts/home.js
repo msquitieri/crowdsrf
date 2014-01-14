5d63998ce337df9d7dd66cbbf2d94cb1c4b7b252
squit <michael.squitieri@gmail.com>
2014-01-14 16:59:39 +0100

    added home controller/route for homepage

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,24 @@
+// $(function() {
+//   handler = Gmaps.build('Google');
+//   handler.buildMap({ provider: {maxZoom: 6}, internal: {id: 'map'}}, function(){
+//     markers = handler.addMarkers([
+//       {
+//         "lat": 0,
+//         "lng": 0,
+//         "picture": {
+//           "url": "https://addons.cdn.mozilla.net/img/uploads/addon_icons/13/13028-64.png",
+//           "width":  36,
+//           "height": 36
+//         },
+//         "infowindow": "hello!"
+//       }
+//     ]);
+//     // handler.bounds.extendWith(markers);
+//     handler.map.centerOn({lat: 40, lng: 73});
+//     // handler.fitMapToBounds();
+//   });
+
+//   $("#center-map-button").click(function() {
+//     console.log("hello!");
+//   });
+// });
